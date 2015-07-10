eab1f27b71c8182285c8718f750450cf05fffe3a
JamesKennethRobinson <Jameskennthrobinson@gmail.com>
2015-07-09 18:44:05 -0700

    (feat) twitter-node stream working in utils

--- server/config.js
+++ server/config.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var Twitter = require('twitter');
 var util = require('./utils.js');
 
 
 var app = express();
 
 app.use(bodyParser.json());
+
+
 
 app.get('/', util.getTwitterData);
 
--- server/utils.js
+++ server/utils.js
@@ -1,7 +1,42 @@
 var Twitter = require('twitter');
 
+////////
+// !!! IMPORTANT !!! --> NEED TO HIDE TWITTER APP DATA BELOW
+////////
 
+var client = new Twitter({
+	consumer_key: 's0E55g1fs108kSjWVycCarT64',
+	consumer_secret: 'LXYNAjWQcJ1UjpTrsPdmqWmdSAv5wTFNdYLhOVELFgFnE5OEXN',
+	access_token_key: '874702442-UH5dCPdQ2tyl6NiqbwPFhyzsFNOYbFDdzQiuC0ar',
+	access_token_secret: 'QLDf9QCxUzMxD7FkXMkTDKSmM5bB3Fe3ypvbw4Gq1GpAv'
+});
 
 exports.getTwitterData = function(req, res){
-	res.send('hello ybas');
+
+
+	// client.get('favorites/list', function(error, tweets, response){
+	//   if(error) throw error;
+	//   console.log(response);  // The favorites. 
+	//   res.send(response);
+	// });
+
+	client.stream('statuses/filter', {track: 'giants'}, function(stream){
+
+		stream.on('data', function(tweet){
+			console.log(tweet)
+		});
+
+		stream.on('error', function(error){
+			throw error;
+		});
+	});
+
 };
+
+
+exports.testFunction = function(req, res){
+	console.log('hello');
+	res.send('hello');
+};
+
+//https://stream.twitter.com/1.1/statuses/filter.json?
