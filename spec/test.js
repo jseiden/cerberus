46fd0e447f1949d1f0a0ce27159316bca9bdac20
jseiden <jseiden@gmail.com>
2015-07-11 15:51:25 -0700

    (init) set up dummy testing suite

---
+++ spec/test.js
@@ -0,0 +1,8 @@
+describe("testing init", function(){
+  it("true equals true", function(){
+    expect(true).toBe(true);
+  });
+  it("false equals false", function(){
+    expect(false).toBe(false);
+  })
+});
