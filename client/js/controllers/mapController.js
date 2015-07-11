5262589f95acc392f458afd6559688be4adb82ba
Justin Fong <justinf715@gmail.com>
2015-07-11 15:00:24 -0700

    (feat) Interface with '/dummy' from server. Obtaining dummy data. Close #16

--- client/js/controllers/mapController.js
+++ client/js/controllers/mapController.js
@@ -1,5 +1,9 @@
 var map = angular.module('app.mapController', []);
 
-map.controller('MapController', function($scope) {
-  
+map.controller('MapController', function($scope, MapService) {
+  MapService.getBeachData()
+  .then(function() {
+    //TODO: Fill out what to do once beach data is obtained
+    //TODO: Stop spinner
+  });
 });
