321fda8b1477647b5172014a68b61c5d082dc95a
Justin Thareja <justin.thareja@gmail.com>
2015-07-11 12:06:59 -0700

    (init)

--- server/config.js
+++ server/config.js
@@ -1,25 +1,19 @@
-//node moduels
+// node modules
 var express = require('express');
 var bodyParser = require('body-parser');
-var request = require('request');
-
-
-//local modules
+// local modules
 var util = require('./utils.js');
 
+var app = express();
+app.use(bodyParser.json());
 
-app = express();
-
-
-//routes
-app.get('/', function(req, res){
+// routes
+app.get('/', function (req, res) {
 	console.log('----', req.body);
 	res.send('hello world');
 });
 
 app.get('/dummy', util.sendDummyData);
-
-
 
 module.exports = app;
 
