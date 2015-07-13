027bdbaa1e5042c3413e5a7ddfd75445d3438de0
JamesKennethRobinson <Jameskennthrobinson@gmail.com>
2015-07-13 11:24:12 -0700

    (feat) required spotId.json into controller

--- server/config.js
+++ server/config.js
@@ -12,8 +12,12 @@ app.use(bodyParser.json());
 //these routes are TBD
 app.get('/', controller.helloWorld);
 app.get('/dummy', controller.sendDummyData);
+
+
 app.get('/dbInit', controller.createDB);
 app.get('/dbRetrieve', controller.retrieveDB);
+
+app.get('/testJSON', controller.testJSON);
 
 module.exports = app;
 
