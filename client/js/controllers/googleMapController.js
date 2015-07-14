97d245c05413067974253889711e2cb006efc01a
jseiden <jseiden@gmail.com>
2015-07-13 18:46:19 -0700

    (feat) connect angular to dummy route. cors module.

--- client/js/controllers/googleMapController.js
+++ client/js/controllers/googleMapController.js
@@ -1,13 +1,15 @@
 var map = angular.module('app.googleMapController', ["ngMap"]);
 
 map.controller('GoogleMapController', function($scope, MapService) {
-  // $scope.getBeachData = function() {
-  //   MapService.getBeachData()
-  //   .then(function() {
-  //     //TODO: Fill out what to do once beach data is obtained
-  //     //TODO: Stop spinner
-  //   });
-  // };
-
-
+  $scope.getBeachData = function() {
+    MapService.getBeachData()
+     .then(function(result){
+       console.log(result.data);
+     })
+    // .then(function() {
+      //TODO: Fill out what to do once beach data is obtained
+      //TODO: Stop spinner
+    // });
+  };
+  $scope.getBeachData();
 });
--- server/config.js
+++ server/config.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var controller = require('./controller.js');
-
-
+var cors = require("cors");
+var morgan = require("morgan");
 
 var app = express();
 
+app.use(cors());
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 
 
-//these routes are for TESTING ONLY 
+//these routes are for TESTING ONLY
 app.get('/dummy', controller.sendDummyData);
 
 
@@ -17,4 +19,3 @@ app.get('/dbRetrieve', controller.retrieve);
 app.get('/dbTestPopulate', controller.populate);
 
 module.exports = app;
-
