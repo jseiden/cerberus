b89dd03e828ec6b8f7cb1af191d702dd23fb24d0
jseiden <jseiden@gmail.com>
2015-07-14 15:22:46 -0700

    (feat) populate markers with db

--- client/js/controllers/googleMapController.js
+++ client/js/controllers/googleMapController.js
@@ -1,10 +1,14 @@
 var map = angular.module('app.googleMapController', ["ngMap"]);
 
 map.controller('GoogleMapController', function($scope, MapService) {
+  console.log("beginning of controller??");
   $scope.getBeachData = function() {
     MapService.getBeachData()
      .then(function(result){
-       console.log(result.data);
+      //  console.log(result.data[0]);
+       $scope.beachData = result.data;
+       console.log($scope.beachData);
+       return result.data;
      })
     // .then(function() {
       //TODO: Fill out what to do once beach data is obtained
