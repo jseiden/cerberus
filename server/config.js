2fb00312b1a847a593bf876bfd471bd4f326924f
JamesKennethRobinson <Jameskennthrobinson@gmail.com>
2015-07-15 16:55:36 -0700

    (refactor) restructured backend fs

--- server/config.js
+++ server/config.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
-var ctlr = require('./controller.js');
 var cors = require("cors");
 var morgan = require("morgan");
 var cron = require('node-schedule');
+
+var ctlr = require('./controller.js');
+var apiUtils = require('./utils/apiUtils.js');
+var crudUtils = require('./utils/crudUtils.js');
 
 var app = express();
 
@@ -13,14 +16,17 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
-//functions to be called on server init
-ctlr.intervalRequest();
+//utils to be called on server init
+apiUtils.updateBeachData();
 
 //these routes are for TESTING ONLY
-app.get('/dbTestPopulate', ctlr.populate);
-app.get('/testMswRequest', ctlr.mswRequest);
-app.get('/dbData', ctlr.sendDbData);
-app.get('/updateDb', ctlr.dbUpdate);
-app.get('/test', ctlr.testSurfData);
+app.get('/writeBeachEntries', crudUtils.writeBeachEntries);
+app.get('/retrieveBeachDatas', crudUtils.retrieveBeachDatas);
+app.get('/beachDataRequests', apiUtils.beachDataReqs);
+
+
+//these routes are for serving static assets
+//app.get('/', ctrl.serveDummyHome)
 
 module.exports = app;
+
--- server/controller.js
+++ server/controller.js
@@ -1,80 +1,19 @@
-var request = require('request');
-var cron = require('node-schedule');
-
-var testData = require('./utils/testData.js');
-var db = require('./db//mongodb-data/config.js');
-var Beach = require('./db/models/beach.js');
-var spotData = require('./utils/json/locationsWithCoords.json');
-
-
-
-
-exports.sendDbData = function (req, res) {
-  	Beach.find({}, function(err, data){
-  		res.send(data);
-  	})
-};
-
-exports.createEntry = function(req, res, data){
-  	var newBeach = Beach({
-			mswId: data.mswId,
-			beachname: data.beachName,
-			lat: data.lat,
-			lon: data.lon,
-			forecastData: ['justin puts the lotion on its skin']
-		});
-
-		newBeach.save(function(err){
-			if (err) throw err;
-			console.log('Beach Entry Created!')
-		});
-};
-
-exports.populate = function(req, res){
-	for (var i=0; i<spotData.length; i++){
-		exports.createEntry(req, res, spotData[i]);
-	}
-};
-
-exports.testSurfData = function(){   
-  console.log('run'); 
-  spotData.forEach(function(ids){
-    var id = ids.mswId;
-    exports.mswRequest(id, function(surfData){
-      exports.dbUpdate(id, surfData)
-    });
-  })
-};
-
-exports.dbUpdate = function(id, data){
-  Beach.findOneAndUpdate({mswId: id}, {forecastData: data}, function(err, beach){
-    if (err) throw err;
-   // console.log('db: ', beach);
-  })
-};
-
-exports.mswRequest = function(id, cb){
-	var endpoint = 'http://magicseaweed.com/api/436cadbb6caccea6e366ed1bf3640257/forecast/?spot_id=' + id.toString();
-	request({
-    method: 'GET', 
-    uri: endpoint
-  },
-  function (error, response, body){
-    if (error){
-      return console.log('request failed: ', error);
-    }
-    cb(body)
-  })
-};
-
-exports.intervalRequest = function(){
-  var rule = new cron.RecurrenceRule();
-  rule.second = 30;
-  cron.scheduleJob(rule, function(){
-    console.log('The answer to life...');
-    //will eventually be exports.mswRequest
-  });
+exports.serveDummyHome = function(req, res){
+  res.send('hello world');
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
