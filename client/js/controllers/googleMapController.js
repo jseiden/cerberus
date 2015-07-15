cb9b7d2738f384e7169eaf5bff746b6b1fef2594
jseiden <jseiden@gmail.com>
2015-07-15 12:09:13 -0700

    (feat) populated map. slight debugging still needed

--- client/js/controllers/googleMapController.js
+++ client/js/controllers/googleMapController.js
@@ -2,18 +2,17 @@ var map = angular.module('app.googleMapController', ["ngMap"]);
 
 map.controller('GoogleMapController', function($scope, MapService) {
   console.log("beginning of controller??");
-  $scope.getBeachData = function() {
+  var markers = [];
+  $scope.getBeachData = function(){
     MapService.getBeachData()
-     .then(function(result){
-      //  console.log(result.data[0]);
-       $scope.beachData = result.data;
-       console.log($scope.beachData);
-       return result.data;
-     })
-    // .then(function() {
-      //TODO: Fill out what to do once beach data is obtained
-      //TODO: Stop spinner
-    // });
+      .then(function(result){
+        $scope.beachData = result.data;
+        // return result.data;
+      })
   };
+  // .then(function() {
+  //     //TODO: Fill out what to do once beach data is obtained
+  //     //TODO: Stop spinner
+  //   // });
   $scope.getBeachData();
 });
