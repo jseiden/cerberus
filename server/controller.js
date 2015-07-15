b0087fe0474c8419572cb97e818299edc02c05be
JamesKennethRobinson <Jameskennthrobinson@gmail.com>
2015-07-15 12:14:22 -0700

    (feat) added (psuedo) cron process for msw request

--- server/controller.js
+++ server/controller.js
@@ -1,18 +1,21 @@
 var request = require('request');
+var cron = require('node-schedule');
+
 var testData = require('./utils/testData.js');
 var db = require('./db//mongodb-data/config.js');
 var Beach = require('./db/models/beach.js');
 var spotData = require('./utils/json/locationsWithCoords.json');
 
-module.exports = {
 
-  sendDbData: function (req, res) {
+
+
+exports.sendDbData = function (req, res) {
   	Beach.find({}, function(err, data){
   		res.send(data);
   	})
-  },
+};
 
-  createEntry: function(req, res, data){
+exports.createEntry = function(req, res, data){
   	var newBeach = Beach({
 			mswId: data.mswId,
 			beachname: data.beachName,
@@ -25,58 +28,53 @@ module.exports = {
 			if (err) throw err;
 			console.log('Beach Entry Created!')
 		});
+};
+
+exports.populate = function(req, res){
+	for (var i=0; i<spotData.length; i++){
+		exports.createEntry(req, res, spotData[i]);
+	}
+};
+
+exports.testSurfData = function(){   
+  console.log('run'); 
+  spotData.forEach(function(ids){
+    var id = ids.mswId;
+    exports.mswRequest(id, function(surfData){
+      exports.dbUpdate(id, surfData)
+    });
+  })
+};
+
+exports.dbUpdate = function(id, data){
+  Beach.findOneAndUpdate({mswId: id}, {forecastData: data}, function(err, beach){
+    if (err) throw err;
+   // console.log('db: ', beach);
+  })
+};
+
+exports.mswRequest = function(id, cb){
+	var endpoint = 'http://magicseaweed.com/api/436cadbb6caccea6e366ed1bf3640257/forecast/?spot_id=' + id.toString();
+	request({
+    method: 'GET', 
+    uri: endpoint
   },
+  function (error, response, body){
+    if (error){
+      return console.log('request failed: ', error);
+    }
+    cb(body)
+  })
+};
 
-  populate: function(req, res){
-  	for (var i=0; i<spotData.length; i++){
-  		module.exports.createEntry(req, res, spotData[i]);
-  	}
-  },
-
-  testSurfData: function(){    
-
-    //WEIRDNESS FOLLOWS --> 
-    // for (var i=0; i<spotData.length; i++){
-    //   var id = spotData[i]['mswId'];
-    //   console.log(id);
-    //   module.exports.mswRequest(id, function(surfData){
-    //     //module.exports.dbUpdate(id, surfData)
-    //     console.log('----', thatId)
-    //   });
-    // }
-
-    spotData.forEach(function(ids){
-      var id = ids.mswId;
-      module.exports.mswRequest(id, function(surfData){
-        module.exports.dbUpdate(id, surfData)
-      });
-    })
-  },
-
-  dbUpdate: function(id, data){
-    Beach.findOneAndUpdate({mswId: id}, {forecastData: data}, function(err, beach){
-      if (err) throw err;
-      //console.log('db: ', beach);
-    })
-    // Beach.findOneAndUpdate({mswId: 162}, {forecastData: ['james puts the lotion on its skin']}, function(err, beach){
-    //   if (err) throw err;
-    //   console.log(beach);
-    // })
-  },
-
-  mswRequest: function(id, cb){
-  	var endpoint = 'http://magicseaweed.com/api/436cadbb6caccea6e366ed1bf3640257/forecast/?spot_id=' + id.toString();
-  	request({
-      method: 'GET', 
-      uri: endpoint
-    },
-    function (error, response, body){
-      if (error){
-        return console.log('request failed: ', error);
-      }
-      cb(body)
-    })
-  }
-
+exports.intervalRequest = function(){
+  var rule = new cron.RecurrenceRule();
+  rule.second = 30;
+  cron.scheduleJob(rule, function(){
+    console.log('The answer to life...');
+    //will eventually be exports.mswRequest
+  });
 }
+
+
 
