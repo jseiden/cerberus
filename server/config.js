6e0d942546e2bb9332c221e9930b96fa4ed5a5f5
JamesKennethRobinson <Jameskennthrobinson@gmail.com>
2015-07-15 12:13:58 -0700

    (feat) added cron process on server init

--- server/config.js
+++ server/config.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
-var controller = require('./controller.js');
+var ctlr = require('./controller.js');
 var cors = require("cors");
 var morgan = require("morgan");
+var cron = require('node-schedule');
 
 var app = express();
 
@@ -12,11 +13,14 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
+//functions to be called on server init
+ctlr.intervalRequest();
+
 //these routes are for TESTING ONLY
-app.get('/dbTestPopulate', controller.populate);
-app.get('/testMswRequest', controller.mswRequest);
-app.get('/dbData', controller.sendDbData);
-app.get('/updateDb', controller.dbUpdate);
-app.get('/test', controller.testSurfData);
+app.get('/dbTestPopulate', ctlr.populate);
+app.get('/testMswRequest', ctlr.mswRequest);
+app.get('/dbData', ctlr.sendDbData);
+app.get('/updateDb', ctlr.dbUpdate);
+app.get('/test', ctlr.testSurfData);
 
 module.exports = app;
