8860406ff3bda8c5699fbf84601d1263ab855c1e
Justin Fong <justinf715@gmail.com>
2015-07-16 12:12:33 -0700

    (feat) Open modals when clicking on markers and send forecast data to modals

--- client/js/controllers/detailsController.js
+++ client/js/controllers/detailsController.js
@@ -1,13 +1,12 @@
 var det = angular.module('app.detailsController', []);
 
-det.controller('DetailsController', function($scope, $modalInstance, items) {
-  $scope.items = items;
-  $scope.selected = {
-    item: $scope.items
-  }
-  $scope.ok = function () {
-    $modalInstance.close($scope.selected.item);
-  };
+det.controller('DetailsController', function($scope, $modalInstance, forecast) {
+  $scope.forecast = forecast;
+  console.log($scope.forecast);
+  // TODO: Template for data selection from modal
+  // $scope.ok = function () {
+  //   $modalInstance.close($scope.selected.forecast);
+  // };
   $scope.cancel = function () {
     $modalInstance.dismiss('cancel');
   };
--- client/js/controllers/googleMapController.js
+++ client/js/controllers/googleMapController.js
@@ -1,6 +1,6 @@
 var map = angular.module('app.googleMapController', ["ngMap"]);
 
-map.controller('GoogleMapController', function($scope, MapService) {
+map.controller('GoogleMapController', function($scope, $modal, MapService) {
   console.log("beginning of controller??");
   var markers = [];
   $scope.getBeachData = function(){
@@ -14,5 +14,28 @@ map.controller('GoogleMapController', function($scope, MapService) {
   //     //TODO: Fill out what to do once beach data is obtained
   //     //TODO: Stop spinner
   //   // });
+
+  // TODO: Add modal animation toggle
+  // $scope.animationsEnabled = true;
+  //
+  // $scope.toggleAnimation = function() {
+  //   $scope.animationsEnabled = !$scope.animationsEnabled;
+  // }
+
+  $scope.open = function() {
+    var context = this;
+    var modalInstance = $modal.open({
+      animation: true,
+      templateUrl: 'detailsModal.html',
+      controller: 'DetailsController',
+      size: 'lg',
+      resolve: {
+        forecast: function () {
+          return context.forecast;
+        }
+      }
+    });
+  };
+
   $scope.getBeachData();
 });
