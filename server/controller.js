de652be1e3ac2251b2f8172351e6b1bb82e5821d
Justin Thareja <justin.thareja@gmail.com>
2015-07-15 18:57:38 -0700

    (refactor) update server to server static assets and index.html

--- server/controller.js
+++ server/controller.js
@@ -1,7 +1,52 @@
-exports.serveDummyRoot = function(req, res){
-  res.send('hello world');
-};
+var request = require('request');
+var testData = require('./utils/testData.js');
+var db = require('./db//mongodb-data/config.js');
+var Beach = require('./db/models/beach.js');
+var spotData = require('./utils/json/locationsWithCoords.json');
+var path = require('path');
 
-//fill in export.function to serve down beach data (crudUtils.js --> retrieveBeachDatas)
+  sendDbData: function (req, res) {
+  	Beach.find({}, function(err, data){
+  		res.send(data);
+  	})
+  },
 
+  createEntry: function(req, res, data){
+  	var newBeach = Beach({
+			mswId: data.mswId,
+			beachname: data.beachName,
+			lat: data.lat,
+			lon: data.lon,
+			forecastData: ['justin puts the lotion on its skin']
+		});
+
+		newBeach.save(function(err){
+			if (err) throw err;
+			console.log('Beach Entry Created!')
+		});
+  },
+
+  populate: function(req, res){
+  	for (var i=0; i<spotData.length; i++){
+  		module.exports.createEntry(req, res, spotData[i]);
+  	}
+  },
+
+  mswRequest: function(req, res){
+  	var endpoint = 'http://magicseaweed.com/api/436cadbb6caccea6e366ed1bf3640257/forecast/?spot_id=10'
+  	request
+  		.get(endpoint)
+  		.on('error', function(err){
+  			console.log(err);
+  		})
+  		.on('response', function(response){
+  			console.log(response.statusCode);
+  		})
+  },
+
+  sendIndex: function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/html/', 'index.html'));
+  }
+
+}
 
