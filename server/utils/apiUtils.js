ec47fc477f1bf9830e1fa94ae5cdea3262b5c399
JamesKennethRobinson <Jameskennthrobinson@gmail.com>
2015-07-16 16:30:07 -0700

    (fix) refactored to handle corrupted db entries due to async

--- server/utils/apiUtils.js
+++ server/utils/apiUtils.js
@@ -1,39 +1,58 @@
 var request = require('request');
+var rp = require('request-promise');
 var cron = require('node-schedule');
 
 var spotData = require('./json/beachData.json');
 var crudUtils = require('./crudUtils');
 
 exports.beachDataReq = function(id, cb){
-	var endpoint = 'http://magicseaweed.com/api/436cadbb6caccea6e366ed1bf3640257/forecast/?spot_id=' + id.toString();
-	request({
-    method: 'GET',
+  var failed = [];
+  // var endpoint = 'http://magicseaweed.com/api/436cadbb6caccea6e366ed1bf3640257/forecast/?spot_id=' + id.toString();
+  var endpoint = 'http://magicseaweed.com/api/436cadbb6caccea6e366ed1bf3640257/forecast/?spot_id=' + id.toString();
+  var options = {
+    method: 'GET', 
     uri: endpoint
-  }, 
-  function (error, response, body){
-    if (error){
-      return console.log('request failed: ', error);
-    }
-    cb(body);
-  })
+  };
+
+  rp(options)
+  //this idealy should not be a callback...should chain in promise format
+    .then(function(data){
+      cb(data)
+    })
+    .catch(function(){
+      failed.push(id);
+    })
+
+  console.log(failed);
 };
 
+
+// exports.beachDataReqs = function(){   
+//   spotData.forEach(function(ids){
+//     var id = ids.mswId;
+//     exports.beachDataReq(id, function(surfData){
+//      crudUtils.beachDatumUpdate(id, surfData);
+//     })
+//   })
+// };
+
+// prototype version
 //needs to be MAJORLY refactored...callback hell :(
 exports.beachDataReqs = function() {   
   spotData.forEach(function(ids) {
     var id = ids.mswId;
-    exports.beachDataReq(id, function(surfData) {
-      crudUtils.beachDataUpdate(id, surfData);
-    });
-  });
+    exports.beachDataReq(id, function(surfData){
+      var timeFiltered = crudUtils.filterBeachDataTime(surfData);
+      crudUtils.beachDatumUpdate(id, timeFiltered);
+    })
+  })
 };
+
 
 exports.updateBeachData = function(){
   var rule = new cron.RecurrenceRule();
   rule.minute = 30;
   cron.scheduleJob(rule, function(){
-    console.log('i like fat CENSORED...');
-    //will eventually be...
     // "export.beachDataRequests"
   });                                               
 };
--- server/utils/crudUtils.js
+++ server/utils/crudUtils.js
@@ -1,22 +1,26 @@
+var _ = require('underscore');
+
 var db = require('../db/mongodb-data/config.js');
 var Beach = require('../db/models/beach.js');
 var apiUtils = require('./apiUtils.js');
 var spotData = require('./json/beachData.json');
 
-exports.writeBeachEntry = function(beachData){
-	var newBeach = Beach({
-		mswId: beachData.mswId,
-		beachname: beachData.beachName,
-		lat: beachData.lat,
-		lon: beachData.lon,
-		forecastData: ['test']
-	});
 
-	newBeach.save(function(err){
-		if (err) throw err;
-		console.log('Beach Entry Created!');
-	});
+exports.writeBeachEntry = function(beachData){
+  	var newBeach = Beach({
+			mswId: beachData.mswId,
+			beachname: beachData.beachName,
+			lat: beachData.lat,
+			lon: beachData.lon,
+			forecastData: ['justin f puts the lotion on its skin']
+		});
+
+		newBeach.save(function(err){
+				if (err) throw err;
+				console.log('Beach Entry Created!')
+			});
 };
+
 
 exports.beachDataUpdate = function(){
 	spotData.forEach(function(spotData){
@@ -24,6 +28,12 @@ exports.beachDataUpdate = function(){
 	})
 };
 
+// working version
+// exports.beachDatumUpdate = function(id, data){
+//   Beach.findOneAndUpdate({mswId: id}, {forecastData: data}, function(err, beach){
+//     if (err) throw err;
+//   })
+// };
 
 exports.beachDatumUpdate = function(id, data){
   Beach.findOneAndUpdate({mswId: id}, {forecastData: data}, function(err, beach){
@@ -31,10 +41,19 @@ exports.beachDatumUpdate = function(id, data){
   })
 };
 
-exports.retrieveAllBeachData = function (cb) {
-	Beach.find({}, function(err, data){
-		cb(data);
-	})
 
-
+exports.retrieveBeachData = function (cb) {
+  	Beach.find({}, function(err, data){
+  		cb(data);
+  	})
 };
+
+exports.filterBeachDataTime = function(data){
+	var parsedData = JSON.parse(data);
+	var time = Math.floor( (Date.now()/1000) );
+	return _.filter(parsedData, function(datum){
+		return datum.timestamp > time && datum.timestamp < (time + 86402);
+	});
+};
+
+
