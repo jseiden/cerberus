63025f8a8a612ddf50028643c9861161529cb150
Justin Thareja <justin.thareja@gmail.com>
2015-07-15 18:57:38 -0700

    (refactor) update server to server static assets and index.html

--- server/config.js
+++ server/config.js
@@ -4,10 +4,11 @@ var request = require('request');
 var cors = require("cors");
 var morgan = require("morgan");
 var cron = require('node-schedule');
-
 var controller = require('./controller.js');
 var apiUtils = require('./utils/apiUtils.js');
 var crudUtils = require('./utils/crudUtils.js');
+var path = require('path');
+
 
 var app = express();
 
@@ -15,19 +16,17 @@ var app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../client'));
 
 //utils to be called on server init
 apiUtils.updateBeachData();
 
+app.get('/', controller.sendIndex);
 //these routes are for TESTING ONLY
 //populates db with one-time beach data (e.g. lat/long, etc)
 app.get('/writeBeachEntries', crudUtils.writeBeachEntries);
 //populates db with msw surf data for respective beach
 app.get('/beachDataRequests', apiUtils.beachDataReqs);
-
-//these routes are for serving static assets
-app.get('/', controller.serveDummyRoot)
-//fill in route to serve down beach data (controller.js -->) 
 
 module.exports = app;
 
