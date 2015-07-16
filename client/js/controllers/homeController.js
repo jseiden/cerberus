267ad7e467b98d7e2866ac3385ed20c3e6666675
Justin Fong <justinf715@gmail.com>
2015-07-15 17:07:44 -0700

    (feat) Adding a modal to the DOM

--- client/js/controllers/homeController.js
+++ client/js/controllers/homeController.js
@@ -1,5 +1,29 @@
 var home = angular.module('app.homeController', []);
 
-home.controller('HomeController', function($scope) {
+home.controller('HomeController', function($scope, $modal) {
+  $scope.animationsEnabled = true;
 
+  $scope.open = function(size) {
+    var modalInstance = $modal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'detailsModal.html',
+      controller: 'DetailsController',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.toggleAnimations = function() {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  }
 });
