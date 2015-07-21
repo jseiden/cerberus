4143ea1fb7847b59f64c6a63ae734998e92e4a2d
Justin Fong <justinf715@gmail.com>
2015-07-20 19:50:08 -0700

    (feat) Add loading screen to map view

--- client/js/controllers/homeController.js
+++ client/js/controllers/homeController.js
@@ -1,5 +1,14 @@
 var home = angular.module('app.homeController', []);
 
-home.controller('HomeController', function($scope, $modal, $log) {
-  
+home.controller('HomeController', function($scope, $modal, $log, $timeout, MapService) {
+  $scope.mapLoaded = false;
+  $scope.animationFinished = false;
+  $scope.$on('map loaded', function() {
+    $timeout(function() {
+      $scope.mapLoaded = true;
+      $timeout(function() {
+        $scope.animationFinished = true;
+      }, 2000)
+    }, 10000)
+  });
 });
