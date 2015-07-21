ecda5139fdc08c70cb0b15fea13ae567a8e1b525
jseiden <jseiden@gmail.com>
2015-07-21 16:52:50 -0700

    (fix) mid rebase commit

--- client/js/controllers/googleMapController.js
+++ client/js/controllers/googleMapController.js
@@ -4,7 +4,7 @@ angular.module('app.googleMapController', [])
     // $scope.spotColors = ['#EBF5FF', '#ADD6FF', '#70B8FF', '#3399FF', '#246BB2'];
     // $scope.spotColors = ['#F0FFFA', '#C2FFEB', '#94FFDB', '#66FFCC', '#3D997A'];
     $scope.spotColors = ['#E6FAF5', '#99EBD6', '#4DDBB8', '#00CC99', '#008F6B'];
-    
+
     $scope.open = function() {
       console.log('scope clicked')
       // TODO: Add modal animation toggle
@@ -28,11 +28,11 @@ angular.module('app.googleMapController', [])
           }
         }
       });
-    };  
+    };
 
     $scope.renderMarkers = function () {
       // All d3 renderings must be done after injecting the d3 library into the controller by calling d3Service.d3()
-      d3Service.d3().then(function(d3) {      
+      d3Service.d3().then(function(d3) {
         var map = new google.maps.Map(d3.select('#map').node(), {
           zoom: 6,
           center: new google.maps.LatLng(36.958, -119.2658)
@@ -40,11 +40,12 @@ angular.module('app.googleMapController', [])
         
         MapService.setMap(map);
         MapService.getBeachData().then(function (beaches) {
-
+          console.log("beaches in gmapController: ", beaches);
+          MapService.setBeachCache(beaches);
           $scope.beaches = beaches;
 
           var overlay = new google.maps.OverlayView();
-          
+
           overlay.onAdd = function () {
             var layer = d3.select(this.getPanes().overlayMouseTarget).append('div')
               .attr('class', 'beaches');
@@ -65,7 +66,7 @@ angular.module('app.googleMapController', [])
                 .attr('r', 4.5)
                 .attr('cx', padding)
                 .attr('cy', padding)
-                .attr('fill', function(d) { 
+                .attr('fill', function(d) {
                   if (!d.value.forecastData.length) {
                     return $scope.spotColors[0];
                   }
@@ -73,7 +74,7 @@ angular.module('app.googleMapController', [])
                     return $scope.spotColors[d.value.forecastData[0].solidRating];
                   }
                 })
-                .attr('forecast', function(d) { 
+                .attr('forecast', function(d) {
                   $scope.forecastData = d.value.forecastData
                   return JSON.stringify(d.value.forecastData);
                 })
--- client/js/services/mapService.js
+++ client/js/services/mapService.js
@@ -2,6 +2,7 @@ angular.module('app.mapService', [])
   .service('MapService', function($http, $rootScope) {
 
     var map;
+    var beachCache;
 
     var getBeachData = function() {
       return $http({
@@ -28,10 +29,34 @@ angular.module('app.mapService', [])
       return map;
     };
 
+    // setting beachCache for ready access without extra ajax calls
+    var setBeachCache = function(beachObj){
+      console.log("argument to setBeachCache: ", beachObj);
+      beachCache = beachObj;
+      console.log("beachCache set to: ", beachCache);
+      $rootScope.$broadcast("beachCacheSet");
+    };
+
+    var getBeachCache = function(){
+      console.log("getBeachCache fired");
+      // if(!beachCache) {
+      //   return null
+      // }
+      console.log("beachCache in getBeachCache: ", beachCache);
+      return beachCache;
+    };
+
+    var zoomToBeach = function(beach){
+      console.log("zoomToBeach fired with ", beach);
+    };
+
     return {
       getBeachData: getBeachData,
       markersLoaded: markersLoaded,
       setMap: setMap,
-      getMap: getMap
+      getMap: getMap,
+      setBeachCache: setBeachCache,
+      getBeachCache: getBeachCache,
+      zoomToBeach: zoomToBeach
     };
   });
