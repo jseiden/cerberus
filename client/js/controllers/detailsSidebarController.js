73a8eca3389694d6d72f0785e05df9e7f81b80c9
Justin Fong <justinf715@gmail.com>
2015-07-28 19:52:36 -0700

    (fix) Details sidebar now loading correctly

--- client/js/controllers/detailsSidebarController.js
+++ client/js/controllers/detailsSidebarController.js
@@ -2,13 +2,22 @@ var sideBar = angular.module('app.detailsSidebarController', []);
 
 sideBar.controller('DetailsSidebarController', function($rootScope, $scope, MapService, AnimationService) {
 
-  var beaches = MapService.getBeachCache();
-  $scope.timeIndex = 0;
-  $scope.selectedBeach = MapService.getCurrentBeach();
-  $scope.timeStamps = MapService.getLocalTimeStamps(beaches);
-  $scope.forecastTime = $scope.timeStamps[$scope.timeIndex];
+  $scope.init = function() {
+    var beaches = MapService.getBeachCache();
+    $scope.timeIndex = 0;
+    $scope.selectedBeach = MapService.getCurrentBeach();
+    $scope.timeStamps = MapService.getLocalTimeStamps(beaches);
+    $scope.forecastTime = $scope.timeStamps[$scope.timeIndex];
+    $scope.detailsTab = false;
+  }
 
-  $scope.detailsTab = false;
+  if (MapService.getBeachCache()) {
+    $scope.init();
+  }
+
+  $scope.$on('beachCacheSet', function() {
+    $scope.init();
+  });
 
   $scope.toggleTab = function() {
     $scope.detailsTab = !$scope.detailsTab;
